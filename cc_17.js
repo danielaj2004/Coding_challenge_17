28c18564db445792e5ded50a3832e58770b823c4
danielaj2004 <danielajoya12@gmail.com>
2025-03-30 21:39:52 -0400

    Task 2- Created salesrep class

--- cc_17.js
+++ cc_17.js
@@ -17,3 +17,23 @@ const customer1 = new Customer("Jane Smith", "Janesmith@gmail.com");
 customer1.addPurchase(150);
 customer1.addPurchase(300);
 console.log(`New customer: ${customer1.name}, Total spent: $${customer1.getTotalSpent()}`);
+
+// Task 2 Create a SalesRep Class
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    }; // constructor for sales rep class
+    addClient(customer) {
+        this.clients.push(customer); // adding clients to sales rep
+    }
+    getClientTotal(name) {
+        const client = this.clients.find(client => client.name === name); // find client by name
+        return client ? client.getTotalSpent() : 0; // return total spent by client
+    }
+}
+// Test Sales Rep
+const salesRep1 = new SalesRep("Robert John");
+salesRep1.addClient(customer1);
+
+console.log(`Sales rep: ${salesRep1.name}, Client: ${customer1.name}, Total spent: $${salesRep1.getClientTotal("Jane Smith")}`);
